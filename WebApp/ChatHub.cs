0c8abb7ce9f7059b2dc005d4ccd94c5a9391b541
liucyi <liucyi@sina.cn>
2016-07-31 14:30:53 +0800

    添加聊天框

--- WebApp/ChatHub.cs
+++ WebApp/ChatHub.cs
@@ -9,18 +9,18 @@ namespace WebApp
 {
     public class ChatHub : Hub
     {
-        ConcurrentDictionary<string, string> OnLineUsers = new ConcurrentDictionary<string, string>();
-        public void Hello()
-        {
-            Clients.All.hello();
 
-        }
         public void Send(string name, string message)
         {
             // Call the addNewMessageToPage method to update clients.
             var time = DateTime.Now;
             Clients.All.addNewMessageToPage(name, message, time);
         }
-      
+
+
+        #region 2
+       
+        #endregion
+
     }
 }
